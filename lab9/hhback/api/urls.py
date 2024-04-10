87fa742c59225da4239467cd755aedea6b1d8d6c
pokonti <pokonti@bk.ru>
2024-04-10 09:25:54 +0500

    lab9

---
+++ lab9/hhback/api/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'api'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('companies/', views.companies),
+    path('companies/<int:pk>/', views.company),
+    path('companies/<int:pk>/vacancies', views.vacancies_by_company),
+    path('vacancies/', views.vacancies),
+    path('vacancies/<int:pk>/', views.vacancy),
+    path('vacancies/top_ten/', views.top_ten_vacancies),
+]
--- lab9/hhback/api/views.py
+++ lab9/hhback/api/views.py
@@ -1,6 +1,8 @@
+import json
 from django.shortcuts import render,get_object_or_404
 from .models import Company, Vacancy
 from django.http.response import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 def index(request):
     companies = Company.objects.all()
@@ -10,14 +12,30 @@ def index(request):
         'vacancies': vacancies,
     })
 
+@csrf_exempt
 def companies(request):
-    companies = Company.objects.all()
-    companies_json = [company.to_json() for company in companies]
-    return JsonResponse(companies_json, safe = False)
+    if request.method == 'GET':
+        companies = Company.objects.all()
+        companies_json = [company.to_json() for company in companies]
+        return JsonResponse(companies_json, safe = False)
+    elif request.method == 'POST':
+        data = json.loads(request.body)
+        company = Company.objects.create(name=data.get("name"), description=data.get("description"), city=data.get("city"), address=data.get("address"))
+        return JsonResponse(company.to_json())
 
+@csrf_exempt
 def company(request,pk):
     company = get_object_or_404(Company, pk=pk)
-    return JsonResponse(company.to_json())
+    if request.method == 'GET':
+        return JsonResponse(company.to_json())
+    elif request.method == 'PUT':
+        data = json.loads(request.body)
+        company.name = data.get("name")
+        company.save()
+        return JsonResponse(company.to_json())
+    elif request.method == 'DELETE':
+        company.delete()
+        return JsonResponse({"deleted": True})
 
 def vacancies_by_company(request, pk):
     company = Company.objects.get(pk=pk)
